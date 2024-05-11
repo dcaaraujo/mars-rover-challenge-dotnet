1ef528828edab9030632cc4db6336c2a03501d93
Christopher Araujo <78472544+dcaaraujo@users.noreply.github.com>
2024-05-12 00:14:39 +0200

    Improved plateau tests to use xunit theories

--- MarsRover.Tests/PlateauTest.cs
+++ MarsRover.Tests/PlateauTest.cs
@@ -4,33 +4,19 @@ public class PlateauTest
 {
     private readonly Plateau _sut = new(5, 5);
 
-    [Fact]
-    public void CoordinatesAreWithinBounds()
-    {
-        bool result = _sut.IsWithinBounds(3, 3);
-        Assert.True(result);
-    }
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(3, 3)]
+    [InlineData(1, 2)]
+    [InlineData(5, 5)]
+    public void CoordinatesAreWithinBounds(int x, int y) => Assert.True(_sut.IsWithinBounds(x, y));
 
-    [Fact]
-    public void XCoordinatesAreOutsideBounds()
-    {
-        bool result = _sut.IsWithinBounds(6, 5);
-        Assert.False(result);
-
-        result = _sut.IsWithinBounds(-1, 5);
-        Assert.False(result);
-    }
-
-
-    [Fact]
-    public void YCoordinatesAreOutsideBounds()
-    {
-        bool result = _sut.IsWithinBounds(3, 6);
-        Assert.False(result);
-
-        result = _sut.IsWithinBounds(3, -1);
-        Assert.False(result);
-    }
+    [Theory]
+    [InlineData(6, 5)]
+    [InlineData(-1, 5)]
+    [InlineData(6, 6)]
+    [InlineData(3, -1)]
+    public void CoordinatesAreOutsideBounds(int x, int y) => Assert.False(_sut.IsWithinBounds(x, y));
 
     [Fact]
     public void ParseFactoryMethodReturnsPlateau()
